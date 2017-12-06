14dedd92f622ab2d367dcaa068d9c69c1b5b17d1
Radoslaw Nadstawny <r.nadstawny@kainos.com>
2017-12-06 12:51:13 +0000

    Simplify InfoRequestHandler promise flow
    With help of bluebird and loadsh.

--- src/infoRequestHandler.ts
+++ src/infoRequestHandler.ts
@@ -1,23 +1,17 @@
+import * as bb from 'bluebird'
 import * as express from 'express'
+import * as mapValues from 'lodash.mapvalues'
 
 import { InfoConfig } from './infoConfig'
 import { loadVersionFile } from './versionFile'
 
 export function infoRequestHandler (config: InfoConfig): express.RequestHandler {
   return (req: express.Request, res: express.Response) => {
-    const entries = Object.entries(config.info)
+    const resolvedEntries = bb.props(mapValues(config.info,
+      value => value.call()
+    ))
 
-    const promises = entries.map((entry) => entry[1].call())
-    const all = Promise.all(promises)
-
-    const downstreamsPromise = all.then(results => {
-      return results.map((result, i) => {
-        return [entries[i][0], result]
-      })
-        .reduce((prev, curr) => ({ ...prev, [curr[0]]: curr[1] }), {})
-    })
-
-    Promise.all([loadVersionFile(), downstreamsPromise])
+    Promise.all([loadVersionFile(), resolvedEntries])
       .then(([versionFile, downstreams]) => {
           const json = {
             build: versionFile,
