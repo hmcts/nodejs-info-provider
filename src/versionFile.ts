1bb1c8e8a03edc96a192b6a0051ababf8655a276
Tim Jacomb <timjacomb1@gmail.com>
2023-05-12 11:34:23 +0100

    Provide version info type

--- src/versionFile.ts
+++ src/versionFile.ts
@@ -1,5 +1,6 @@
 import { readFile } from 'node:fs/promises';
 import * as yaml from 'js-yaml';
+import { VersionInfo } from './versionInfo';
 
 export function loadVersionFile(): Promise<object> {
   const versionFilePath = `${process.env.NODE_PATH || '.'}/version`;
@@ -7,5 +8,5 @@ export function loadVersionFile(): Promise<object> {
     .then((rawVersionFile: Buffer) => yaml.load(rawVersionFile.toString()))
     .catch(() => {
       return { version: 'unknown' };
-    }) as Promise<object>;
+    }) as Promise<VersionInfo>;
 }
